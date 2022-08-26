8adec54666225fb1da95f81aa2faa768ab5c0908
Tony Starikov <tony.starikov.1992@gmail.com>
2022-08-26 16:33:47 +0300

    Edit categories migration

--- database/migrations/2022_02_07_165122_create_categories_table.php
+++ database/migrations/2022_02_07_165122_create_categories_table.php
@@ -16,8 +16,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name_ua')->nullable();
-            $table->string('name_ru')->nullable();
+            $table->string('name_ua');
+            $table->string('name_ru');
             $table->string('slug')->unique();
             $table->text('title')->nullable();
             $table->text('description')->nullable();
